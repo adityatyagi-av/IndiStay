8a910db4a9324de989f27908332a4f0c72318dfa
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-11-19 23:48:12 +0530

    Added sidebar component

---
+++ components/sidebar.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const Sidebar = () => {
+  return (
+    <div className="space-y-4 py-4 flex flex-col h-full bg-gray-800 text-white">
+      hey
+    </div>
+  )
+}
+
+export default Sidebar
